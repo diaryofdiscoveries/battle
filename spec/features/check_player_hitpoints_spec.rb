d50cc2e2ae0e61577d3c07493ec28c536eabf154
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-13 15:32:37 +0100

    Add Test helper method

--- spec/features/check_player_hitpoints_spec.rb
+++ spec/features/check_player_hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature 'check player hitpoints' do
   scenario 'player hit points should show on page' do
-    visit('/')
-    fill_in :player_1_name, with: 'Thomas'
-    fill_in :player_2_name, with: 'Zoe'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Zoe: 60HP'
   end
 end
